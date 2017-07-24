ca77c729f423084f6d48e823e3f29e52a8c29a82
TheOnly8Z <888888zombies@gmail.com>
2017-07-24 13:52:59 -0400

    Scripts

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+	Character
+
+	Base class for all characters, including the player, different enemy types.
+ 
+*/
+public class Character : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
